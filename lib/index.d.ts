e8b33fb12d8e0e0f5c19139300d6a49df05429d7
iamcco <ooiss@qq.com>
2019-08-11 16:07:38 +0800

    fix(filetype): add eruby and HTML (EEx) closes #8 #7

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const CSS_LANGUAGES: string[];
-export declare const JS_LANGUAGES: string[];
-export declare const HTML_LANGUAGES: string[];
-export declare const LANGUAGES: string[];
-export declare function activate(): Promise<void>;
-export declare function deactivate(): Thenable<void>;
