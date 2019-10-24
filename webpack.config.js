d52455ae830babd8aa4b872e0a393f4bf3b1a5ef
iamcco <ooiss@qq.com>
2019-10-24 12:30:22 +0800

    chore(package): use webpack instead

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: {
+    index: './src/index.ts',
+  },
+  target: 'node',
+  mode: 'none',
+  resolve: {
+    mainFields: ['module', 'main'],
+    extensions: ['.js', '.ts']
+  },
+  externals: {
+    'coc.nvim': 'commonjs coc.nvim'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'out'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs'
+  },
+  plugins: [],
+  node: {
+    __dirname: false,
+    __filename: false
+  }
+}
